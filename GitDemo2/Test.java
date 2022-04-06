31d02d5be89175af932c9d29efe999c866eb4586
TAMMANA <sai-lakshmi-niharika.tammana@capgemini.com>
2022-04-06 11:47:39 +0530

    Branch1 and branch2 merged files commited

---
+++ GitDemo2/Test.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+/**
+ * Welcome to Capgemini!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Capgemini!" );
+    }
+}
